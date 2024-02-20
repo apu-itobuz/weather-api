392315328afacd7b32925f9b8a3e502a1945b012
itobuz <itobuz@itobuzs-MacBook-Air.local>
2024-02-20 15:28:04 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+function searchInput() {
+  const inputLocation = document.getElementById("text").value;
+  const apiKey = "0c80b2b56f1943ada19100744230103";
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${inputLocation}&aqi=no`;
+  // console.log(apiUrl);
+  const locationText = document.querySelector(".location");
+  console.dir(locationText);
+  const tempShow = document.querySelector(".temp");
+  const conditionTitle = document.querySelector(".title");
+  const sectionMain = document.getElementsByClassName("section-main")[0];
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const placeName = data.location.name;
+      const temparature = data.current.temp_c;
+      const condition = data.current.condition.text;
+      const currentDate = new Date().toLocaleDateString("en-US", {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+      });
+      locationText.innerText = placeName;
+      tempShow.innerText = temparature;
+      conditionTitle.innerText = condition;
+      sectionMain.classList.remove("day", "night", "rainy");
+      if (condition.toLowerCase().includes("sunny")) {
+        sectionMain.classList.add("day");
+      } else if (condition.toLowerCase().includes(["cloud", "Partly cloudy"])) {
+        sectionMain.classList.add("night");
+      } else sectionMain.classList.add("rainy");
+    });
+}
